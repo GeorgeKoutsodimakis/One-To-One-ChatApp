e1188a03deb634f33a252d94d49721dddcfe9718
Georgios Koutsodimakis <koutsodimakisgeo@gmail.com>
2017-09-07 19:11:38 +0300

    ionChat first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,44 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { ChatPage } from '../pages/chat/chat';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
-})
-export class AppModule {}
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCE88qJZ-sAcHxWohNbRiDnl3_n3H_kvXI",
+  authDomain: "simplechat-176f9.firebaseapp.com",
+  databaseURL: "https://simplechat-176f9.firebaseio.com",
+  projectId: "simplechat-176f9",
+  storageBucket: "",
+  messagingSenderId: "923280181379"
+};
+
+
+  @NgModule({
+    declarations: [
+      MyApp,
+      HomePage,
+      ChatPage,
+
+    ],
+    imports: [
+      BrowserModule,
+      IonicModule.forRoot(MyApp),
+      AngularFireModule.initializeApp(config),
+      AngularFireDatabaseModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+      MyApp,
+      HomePage,
+      ChatPage
+    ],
+    providers: [
+      StatusBar,
+      SplashScreen,
+      {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ]
+  })
+  export class AppModule {}
